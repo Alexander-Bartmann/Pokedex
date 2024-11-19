e8f82f9d21d04899525c91f3bffa46696c7c79d1
Alexander-Bartmann <seirex.mb@gmail.com>
2024-11-19 15:55:09 +0100

    ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
-const baseURL = "https://pokeapi.co/api/v2/pokemon?limit=30&offset=0"
-const allPokemonsArray = [];
-let htmlContent = "";
+let offset = 0;
+const limit = 30;
+let baseURL = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+let currentPokemon = 0;
+
 
 function init() {
     loading();
@@ -22,14 +24,112 @@ function loading() {
 }
 
 async function renderAllPokemons() {
-    htmlContent = "";
+    const content = document.getElementById("content");
+    let htmlContent = '';
     let response = await fetch(baseURL);
     let responseAsJson = await response.json();
     for (let i = 0; i < responseAsJson.results.length; i++) {
-        let pokemon = responseAsJson.results[i];
-        let pokemonData = await fetch(pokemon.url);
-        let pokemonJson = await pokemonData.json();
-        htmlContent += getPokemonTemplate(pokemonJson);
+        const pokemon = responseAsJson.results[i];
+        htmlContent += await getPokemonHTML(pokemon);
     }
-    document.getElementById("content").innerHTML = htmlContent;
+    content.innerHTML += htmlContent;
 }
+
+async function getPokemonHTML(pokemon) {
+    const pokemonData = await fetch(pokemon.url);
+    const pokemonJson = await pokemonData.json();
+    const typeColors = getTypeColors(pokemonJson);
+    return getPokemonTemplate(pokemonJson, typeColors);
+}
+
+function getTypeColors(pokemonJson) {
+    return pokemonJson.types.reduce((accumulator, type) => {
+        accumulator[type.type.name] = getTypeColor(type.type.name);
+        return accumulator;
+    }, {});
+}
+
+async function loadMorePokemons() {
+    offset += limit;
+    baseURL = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+    await renderAllPokemons();
+}
+
+async function renderDetailCard(pokemon) {
+    const detail = document.getElementById('selectedPokemon');
+    detail.classList.remove('d-none');
+    let typeList = "";
+    for (let i = 0; i < pokemon.types.length; i++) {
+        typeList += `<span style="background-color: ${getTypeColor(pokemon.types[i].type.name)}">${pokemon.types[i].type.name}</span> `;
+    }
+    const sprite = pokemon.sprites.versions["generation-v"]["black-white"].animated.front_default || pokemon.sprites.front_default;
+    const evoChainHTML = await getEvolutionImages(pokemon.species.url);
+    detail.innerHTML = getCardTemplate(pokemon, typeList, sprite, evoChainHTML);
+    currentPokemon = pokemon.id;
+}
+
+function closeDetail() {
+    const detail = document.getElementById('selectedPokemon');
+    detail.classList.add('d-none');
+    detail.innerHTML = "";
+}
+
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        section.classList.add('hidden');
+    });
+    const activeSection = document.getElementById(sectionId);
+    if (activeSection) {
+        activeSection.classList.remove('hidden');
+    }
+}
+
+async function getEvolutionImages(speciesUrl) {
+    const speciesResponse = await fetch(speciesUrl);
+    const speciesData = await speciesResponse.json();
+    const evolutionChainResponse = await fetch(speciesData.evolution_chain.url);
+    const evolutionChainData = await evolutionChainResponse.json();    
+    let evoChainHTML = '';
+    let currentEvo = evolutionChainData.chain;
+    for (let count = 0; currentEvo && count < 3; count++) {
+        const pokemonId = currentEvo.species.url.split('/')[6];
+        evoChainHTML += await getPokemonImageHTML(pokemonId, currentEvo.evolves_to.length > 0);        
+        currentEvo = currentEvo.evolves_to[0];
+    }
+    return evoChainHTML;
+}
+
+async function getPokemonImageHTML(pokemonId, hasEvolvesTo) {
+    const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`);
+    const pokemonData = await pokemonResponse.json();
+    return `
+        <div class="evolution-image-container">
+            <img src="${pokemonData.sprites.front_default}" alt="Evolution Image" class="evolution-image">
+            ${hasEvolvesTo ? '<span class="arrow"> >> </span>' : ''}
+        </div>
+    `;
+}
+
+async function searchPokemons() {
+    const search = document.getElementById("searchField").value.toLowerCase();
+    const content = document.getElementById("content");
+    if (search.length < 2) {
+        content.innerHTML = "";
+        renderAllPokemons();
+        return;
+    }
+    const response = await fetch(baseURL);
+    const data = await response.json();
+    const filteredPokemons = data.results.filter(pokemon => pokemon.name.toLowerCase().includes(search));
+    content.innerHTML = "";
+    content.innerHTML = await Promise.all(filteredPokemons.map(pokemon => getPokemonHTML(pokemon)));
+}
+
+
+
+
+
+
+
+
--- assets/js/template.js
+++ assets/js/template.js
@@ -17,28 +17,122 @@ function getHeaderTemplate() {
                     <textPath href="#arcPath" startOffset="60%" font-size="64">X</textPath>
                 </text>
             </svg>
-
-
         </div>
         <div>
-            <input type="text" class"searchfield">
+            <input type="text" id="searchField" class="searchfield" placeholder="Search..." oninput="searchPokemons()">
         </div>
     `;
 }
 
-function getPokemonTemplate(pokemon) {
+function getPokemonTemplate(pokemon, typeColors) {
     return `
-        <div class="pokemon_card">
-            <div>
+        <div class="pokemon_card" onclick="renderDetailCard(${JSON.stringify(pokemon).replace(/"/g, '&quot;')}, ${JSON.stringify(typeColors).replace(/"/g, '&quot;')})">
+            <div style="display: flex; gap: 16px; justify-content: space-around;">
                 <h3>#${pokemon.id}</h3>
                 <h3>${pokemon.name}</h3>
             </div>
-            <div class="pokemon_card_img">
+            <div class="pokemon_card_img" style="background-color: ${typeColors[pokemon.types[0].type.name]};">
                 <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
             </div>
             <div class="pokemon_types">
-                
+                <span style="background-color: ${typeColors[pokemon.types[0].type.name]}">${pokemon.types[0].type.name}</span>
+                ${pokemon.types[1] ? `<span style="background-color: ${typeColors[pokemon.types[1].type.name]}">${pokemon.types[1].type.name}</span>` : ""}
             </div>
         </div>
     `;
 }
+
+
+function getCardTemplate(pokemon, typeList, sprite, evoChainHTML) {
+    return `
+        <div class="selectedPokemon" onclick="closeDetail()" style="display: flex; gap: 16px; justify-content: space-around;">
+            <div class="openCard">
+                <div class="card_details bg">
+                    <div>
+                        <div class="display">
+                            <h2>#${pokemon.id} - ${pokemon.name}</h2>
+                            <button onclick="closeDetail()">X</button>
+                        </div>
+                        <div class="card_details_img" style="background-color: ${getTypeColor(pokemon.types[0].type.name)};">
+                            <img src="${sprite}" alt="${pokemon.name}">
+                        </div>
+                        <div class="pokemon_types">
+                            <p>${typeList}</p>
+                        </div>
+                    </div>
+                    <div>                     
+                        <div class="container_buttons">
+                            <button onclick="showSection('main'); event.stopPropagation();">Main</button>
+                            <button onclick="showSection('stats'); event.stopPropagation();">Stats</button>
+                            <button onclick="showSection('evoChain'); event.stopPropagation();">Evo Chain</button>
+                        </div>
+                        <div id="main" class="section">
+                            <table>
+                                <tr>
+                                    <td><strong>Height: </strong></td>
+                                    <td><strong>Weight:</strong></td>
+                                    <td><strong>Base Experience:</strong></td>
+                                    <td><strong>Abilities:</strong></td>
+                                </tr>
+                                <tr>
+                                    <td>${pokemon.height} m</td>
+                                    <td>${pokemon.weight} kg</td>
+                                    <td>${pokemon.base_experience}</td>
+                                    <td>${pokemon.abilities[0].ability.name},${pokemon.abilities[1].ability.name}</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div id="stats" class="section hidden">
+                            <table>
+                                <tr>
+                                    <td>hp</td>
+                                    <td>attack</td>
+                                    <td>defense</td>
+                                    <td>special-attack</td>
+                                    <td>special-defense</td>
+                                    <td>speed</td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="stat-bar">
+                                            <div class="stat-bar-inner" style="width: ${pokemon.stats[0].base_stat}%;">${pokemon.stats[0].base_stat}</div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="stat-bar">
+                                            <div class="stat-bar-inner" style="width: ${pokemon.stats[1].base_stat}%;">${pokemon.stats[1].base_stat}</div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="stat-bar">
+                                            <div class="stat-bar-inner" style="width: ${pokemon.stats[2].base_stat}%;">${pokemon.stats[2].base_stat}</div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="stat-bar">
+                                            <div class="stat-bar-inner" style="width: ${pokemon.stats[3].base_stat}%;">${pokemon.stats[3].base_stat}</div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="stat-bar">
+                                            <div class="stat-bar-inner" style="width: ${pokemon.stats[4].base_stat}%;">${pokemon.stats[4].base_stat}</div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="stat-bar">
+                                            <div class="stat-bar-inner" style="width: ${pokemon.stats[5].base_stat}%;">${pokemon.stats[5].base_stat}</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div id="evoChain" class="section hidden">
+                            ${evoChainHTML}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
